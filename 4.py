c89e69c1ce9cfe9b1b944a8eae9cc7f21775b662
josh874 <82136125+josh874@users.noreply.github.com>
2022-12-26 01:55:10 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,14 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
